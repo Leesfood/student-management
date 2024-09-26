7cdd3983aa2d5073a5ae41515784e79afa62e407
leesfood <notifications@leesfood.com>
2024-09-26 08:44:09 +0700

    update

--- django_project/settings.py
+++ django_project/settings.py
@@ -26,8 +26,10 @@ SECRET_KEY = config("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 AGE = config("AGE", default=25, cast=int)
-ALLOWED_HOSTS = config("ALLOWED_HOSTS").split(",")
+ALLOWED_HOSTS = ['*']
 
+import logging
+logging.warning(f"ALLOWED_HOSTS: {ALLOWED_HOSTS}")
 
 
 
